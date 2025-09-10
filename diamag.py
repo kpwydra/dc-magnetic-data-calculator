1651ba10cd6397ed5ea43c1379fcc21df45e1918
Sevelantis <34632434+Sevelantis@users.noreply.github.com>
2025-09-10 20:52:33 +0200

    Update diamag.py

--- diamag.py
+++ diamag.py
@@ -6,18 +6,3 @@ def calc_diamag_contrib(formula: str):
     
     return {element: int(count) for element, count in matches}
 
-# elements = {
-#     "C": "carbon",
-#     "H": "hydrogen"
-# }
-
-# element_symbol_list = []
-# for element in compound_formula:
-#     element_coefficient = ''
-#     if element in elements:
-#         element_symbol_list.append(element)
-#     elif element not in elements:
-#         element_coefficient += "{element}"
-#         element_symbol_list.append(element_coefficient)
-
-# print(element_symbol_list)
