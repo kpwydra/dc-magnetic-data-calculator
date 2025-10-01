670594d30521e4d9b88a66f03c28bb295f74daa7
Sevelantis <34632434+Sevelantis@users.noreply.github.com>
2025-10-02 00:53:56 +0200

    Update test_diamag.py

--- tests/unit/test_diamag.py
+++ tests/unit/test_diamag.py
@@ -1,10 +1,10 @@
 from diamag import calc_diamag_contr
-from tests.unit.test_data import CALC_DIAMAG_CONTR_TEST_CASES
+from tests.unit.test_data import MOLECULES_DATA
 
 
 def test_calc_diamag_contr():
     """Verifies diamag calculations are correct."""
 
-    for case in CALC_DIAMAG_CONTR_TEST_CASES:
-        result = calc_diamag_contr(case["data"])
-        assert case["expected"] == round(result, 2)
+    for molecule in MOLECULES_DATA:
+        result = calc_diamag_contr(molecule["elements"])
+        assert molecule["diamag"] == round(result, 2)
